cedc6d1fd99df9e826fb3705ca77296c8113a3cf
tkdgh3457 <tkdgh3457@naver.com>
2024-05-13 09:59:56 +0900

    https://github.com/tkdgh3457/teamproject-2

---
+++ src/test/java/com/tjoeun/shop/TeamprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjoeun.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
